3f819923f473323f198a82ce96c040bdb2db5688
Uziel <urlaweblink@gmail.com>
2023-01-13 10:40:41 -0500

    Update

--- Week1/Monday/src/Mon.java
+++ Week1/Monday/src/Mon.java
@@ -1,5 +1,9 @@
+package Week1.Monday.src;
+
 public class Mon {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("I am in the main method");
+        System.out.println("The value of args is " + args);
     }
 }
