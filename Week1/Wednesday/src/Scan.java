e8d911ee643594ad8daae3bf73de569e082d044e
Uziel <urlaweblink@gmail.com>
2023-01-13 23:58:19 -0500

    Added Scanner

--- Week1/Wednesday/src/Scan.java
+++ Week1/Wednesday/src/Scan.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
+
 public class Scan {
-    // placeholder
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.println("Please enter number of bottles");
+            int bottles = in.nextInt();
+            System.out.println("Please enter price");
+            double price = in.nextDouble();
+            double subTotal = price * bottles;
+            System.out.println("Your subtotal is $" + subTotal);
+            System.out.println("Tax $" + (subTotal * 0.07));
+            double total = subTotal + (subTotal * 0.07);
+            System.out.println("Your total is $" + total);
+        }
+
+    }
 }
