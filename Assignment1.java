bfc12c48bc27568568246b22e0a736c0e189f088
Uziel <urlaweblink@gmail.com>
2023-01-29 01:45:22 -0500

    Eliminating test

--- Assignment1.java
+++ Assignment1.java
@@ -6,7 +6,6 @@ public class Assignment1 {
 
                 // Problem 1
 
-
                 System.out.println("Please enter the number of Toaster");
                 int toasterNum = in.nextInt();
                 System.out.println("Please enter the number of Hair Dryer");
