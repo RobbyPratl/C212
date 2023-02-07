1a091acc16cb58dcf2873d819eaa49d53c57a07a
Uziel <urlaweblink@gmail.com>
2023-02-06 19:23:29 -0500

    Monday Notes

--- Assignments/Assignment3/Problem1.java
+++ Assignments/Assignment3/Problem1.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Problem1 {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int sum = 0;
+        System.out.println("Please enter a number");
+        int input = in.nextInt();
+        if (input == 0) {
+            System.out.println("Output: 0");
+        } else if (input == 1) {
+            System.out.println("Output: 1");
+        } else {
+            for (int i = 0; i < input; i++) {
+                System.out.println(i);
+            }
+            System.out.println(sum);
+        }
 
     }
 }
---
+++ Week5/Mon/Mon.java
@@ -0,0 +1,38 @@
+public class Mon {
+
+    /**
+     * 
+     * @param sideLength
+     * @return
+     */
+    public static double cubeVolume(double sideLength) {
+        double volume = sideLength * sideLength * sideLength;
+        return volume;
+    }
+
+    public static void modifyValue(int n) {
+        n++;
+        System.out.println("In modify value n = " + n);
+    }
+
+    public static int sign(double x) {
+        if (x < 0) {
+            return -1;
+        } else if (x > 0) {
+            return 1;
+        } else {
+
+        }
+    }
+
+    public static void main(String[] args) {
+        double side = 5;
+        double volume = cubeVolume(side);
+        System.out.println(volume);
+
+        int m = 10;
+        System.out.println("Before n= " + m);
+        modifyValue(m);
+        System.out.println("After n= " + m);
+    }
+}
