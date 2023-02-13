6872c92bfb45835ee9d08d8adaf1e08b6b807374
Uziel <urlaweblink@gmail.com>
2023-02-13 09:10:41 -0500

    Starting up 2

---
+++ Assignments/Assignment4/P1.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class P1 {
+
+    public static boolean allTheSame(int x, int y, int z) {
+        if (x == y && y == z && x == z) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean allDifferent(int x, int y, int z) {
+        if (x != y && y != z && x != z) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static boolean sorted(int x, int y, int z) {
+        if (x <= y && y <= z) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter three interger values:");
+        int num1 = in.nextInt();
+        int num2 = in.nextInt();
+        int num3 = in.nextInt();
+
+        System.out.println("allTheSame: " + allTheSame(num1, num2, num3));
+        System.out.println("allDifferent: " + allDifferent(num1, num2, num3));
+        System.out.println("Sorted: " + sorted(num1, num2, num3));
+
+    }
+}
