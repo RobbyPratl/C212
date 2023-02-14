96c454790ed387d5ffddf74214b3782da79eabe2
Uziel <urlaweblink@gmail.com>
2023-02-14 00:04:25 -0500

    Updating p2

--- Assignments/Assignment4/P2.java
+++ Assignments/Assignment4/P2.java
@@ -1,3 +1,23 @@
+
 public class P2 {
-    // pubic static
+    public static int findPostionNum(String str) {
+        // this will be the helper method we use to find the number not the
+
+    }
+
+    public static int numCompare(String str1, String str2) {
+        // int num1 = Integer.parseInt(str1.substring(4));
+        // int num2 = Integer.parseInt(str2.substring(4));
+        // Cant use the above because it's too "advanced"
+
+    }
+
+    public static void main(String[] args) {
+        // Scanner in = new Scanner(System.in);
+        // System.out.println("PLease enter the two files and number together (Example:
+        // file1)");
+        String str1 = "file1";
+        String str2 = "file2";
+        numCompare(str1, str2);
+    }
 }
