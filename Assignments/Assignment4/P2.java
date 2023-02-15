d23c71d25e3cccc53cbf6b5a5a141304bc905033
Uziel <urlaweblink@gmail.com>
2023-02-14 20:08:05 -0500

    Finished problem2

--- Assignments/Assignment4/P2.java
+++ Assignments/Assignment4/P2.java
@@ -1,23 +1,46 @@
 
 public class P2 {
     public static int findPostionNum(String str) {
-        // this will be the helper method we use to find the number not the
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '1' || str.charAt(i) == '2' || str.charAt(i) == '3'
+                    || str.charAt(i) == '4' || str.charAt(i) == '5' || str.charAt(i) == '6'
+                    || str.charAt(i) == '7' || str.charAt(i) == '8' || str.charAt(i) == '9'
+                    || str.charAt(i) == '0') {
+                return i;
+            }
+        }
+        return -1;
 
     }
 
     public static int numCompare(String str1, String str2) {
-        // int num1 = Integer.parseInt(str1.substring(4));
-        // int num2 = Integer.parseInt(str2.substring(4));
-        // Cant use the above because it's too "advanced"
-
+        // return as a postive number if str1 is greater than str2
+        // return as a negative number if str1 is less than str2
+        // return as 0 if str1 is equal to str2
+        int pos1 = findPostionNum(str1);
+        int pos2 = findPostionNum(str2);
+        if (pos1 == -1 && pos2 == -1) {
+            return 0;
+        } else if (pos1 == -1) {
+            return -1;
+        } else if (pos2 == -1) {
+            return 1;
+        } else {
+            int num1 = Integer.parseInt(str1.substring(pos1));
+            int num2 = Integer.parseInt(str2.substring(pos2));
+            if (num1 > num2) {
+                return 1;
+            } else if (num1 < num2) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
     }
 
     public static void main(String[] args) {
-        // Scanner in = new Scanner(System.in);
-        // System.out.println("PLease enter the two files and number together (Example:
-        // file1)");
         String str1 = "file1";
         String str2 = "file2";
-        numCompare(str1, str2);
+        System.out.println(numCompare(str1, str2));
     }
 }
