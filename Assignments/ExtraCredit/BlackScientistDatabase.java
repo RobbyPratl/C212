8b5e755a03d6ce95084338a1c830d63223c2e19d
Uziel <urlaweblink@gmail.com>
2023-02-28 01:47:07 -0500

    Finished extra credit assignment

--- Assignments/ExtraCredit/BlackScientistDatabase.java
+++ Assignments/ExtraCredit/BlackScientistDatabase.java
@@ -51,56 +51,6 @@ public class BlackScientistDatabase {
     }
 
     public static void main(String[] args) {
-        /*
-         * 
-         * Dr. Mae Jemison
-         * Born in 1956
-         * Research fields: astronautics, engineering, and medicine
-         * Jemison was the first Black woman to travel in space, serving as a mission
-         * specialist aboard the space shuttle Endeavour in 1992. She also established
-         * the Jemison Group, a company that uses technology to solve social and
-         * environmental problems.
-         * 
-         * 
-         * Dr. Shirley Ann Jackson
-         * Born in 1946
-         * Research fields: physics, materials science, and telecommunications
-         * Jackson is a theoretical physicist who conducted extensive research on
-         * condensed matter physics, which involves the study of the properties of
-         * solids and liquids. She is also a pioneer in telecommunications research and
-         * is credited with developing the technology that made Caller ID and Call
-         * Waiting possible.
-         * 
-         * 
-         * Dr. Percy Julian
-         * Born in 1899
-         * Research fields: organic chemistry, synthesis of medicinal compounds, and
-         * natural product chemistry
-         * Julian was an accomplished chemist who made many significant contributions to
-         * the field of organic chemistry. He synthesized important medicinal compounds,
-         * including a drug used to treat glaucoma, and also developed a way to extract
-         * chemicals from soybeans, leading to the development of steroid medications.
-         * 
-         * 
-         * Dr. Neil deGrasse Tyson
-         * Born in 1958
-         * Research fields: astrophysics, astronomy, and science communication
-         * Tyson is a renowned astrophysicist who has made significant contributions to
-         * the study of galaxies, stars, and black holes. He is also a prolific science
-         * communicator who has hosted numerous television shows and written several
-         * popular books about space and science.
-         * 
-         * 
-         * Dr. Patricia Bath
-         * Born in 1942
-         * Research fields: ophthalmology, laser technology, and health disparities
-         * Bath was an ophthalmologist who invented a device for removing cataracts
-         * using lasers, making the procedure more precise and less invasive. She was
-         * also a passionate advocate for health equity and worked to improve access to
-         * eye care for underserved communities.
-         * 
-         */
-
         Scanner input = new Scanner(System.in);
         System.out.println(
                 "Welcome to the database of Black scientists! Please enter text for the following filter criteria, or press Enter to skip filtering by that criteria.");
@@ -116,21 +66,23 @@ public class BlackScientistDatabase {
                 { "Dr. Shirley Ann Jackson", "1946", "physics, materials science, and telecommunications",
                         "Jackson is a theoretical physicist who conducted extensive research on condensed matter physics, which involves the study of the properties of solids and liquids. She is also a pioneer in telecommunications research and is credited with developing the technology that made Caller ID and Call Waiting possible." },
                 { "Dr. Percy Julian", "1899",
-                        "organic chemistry, synthesis of medicinal compounds, and natural product chemistry" },
-                { "Dr. Neil deGrasse Tyson", "1958", "astrophysics, astronomy, and science communication" },
-                { "Dr. Patricia Bath", "1942", "ophthalmology, laser technology, and health disparities" } };
+                        "organic chemistry, synthesis of medicinal compounds, and natural product chemistry",
+                        "Julian was an accomplished chemist who made many significant contributions to the field of organic chemistry. He synthesized important medicinal compounds, including a drug used to treat glaucoma, and also developed a way to extract chemicals from soybeans, leading to the development of steroid medications." },
+                { "Dr. Neil deGrasse Tyson", "1958", "astrophysics, astronomy, and science communication",
+                        "Tyson is a renowned astrophysicist who has made significant contributions to the study of galaxies, stars, and black holes. He is also a prolific science communicator who has hosted numerous television shows and written several popular books about space and science." },
+                { "Dr. Patricia Bath", "1942", "ophthalmology, laser technology, and health disparities",
+                        "Bath was an ophthalmologist who invented a device for removing cataracts using lasers, making the procedure more precise and less invasive. She was also a passionate advocate for health equity and worked to improve access to eye care for underserved communities." } };
 
         String[][] result = findScientist(scientists, name, byear, field);
-
         for (int i = 0; i < result.length; i++) {
             if (result[i][0] != null) {
-                System.out.println(result[i][0]);
-                System.out.println("Born in " + result[i][1]);
-                System.out.println("Research fields: " + result[i][2]);
+                System.out.println("Name: " + result[i][0]);
+                System.out.println("Birth year: " + result[i][1]);
+                System.out.println("Field: " + result[i][2]);
+                System.out.println("Contribution: " + scientists[i][3]);
                 System.out.println();
             }
         }
-
         input.close();
     }
 }
