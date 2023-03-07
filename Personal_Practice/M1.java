392f6f9a0acf2ac3d69dc6e40d05cb03780702c6
Uziel <uzielriveralopez3@gmail.com>
2023-03-06 20:56:01 -0500

    Finished Party practice

--- Personal_Practice/M1.java
+++ Personal_Practice/M1.java
@@ -1,5 +1,3 @@
-package Personal_Practice;
-
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -13,12 +11,10 @@ public class M1 {
 
     public static void removePerson(ArrayList<String> list, String notInvited) {
         for (int i = 0; i < list.size(); i++) {
-            if (list.get(i) == notInvited) {
+            if (list.get(i).equals(notInvited)) {
                 list.remove(i);
-                System.out.println("You are not invited");
             }
         }
-        readList(list);
     }
 
     public static void main(String[] args) {
@@ -35,11 +31,13 @@ public class M1 {
         System.out.println("Did you mess up on the inivitation? (Y/N)");
         readList(list);
         String response = in.nextLine();
-        if (response.equals("y")) {
+        if (response.toLowerCase().equals("y")) {
             System.out.println("Who is the person that's not invited?");
             String notInvited = in.nextLine();
             removePerson(list, notInvited);
-        } else if (response.equals("n")) {
+            System.out.println("Here are the people invited!");
+            readList(list);
+        } else if (response.toLowerCase().equals("n")) {
             System.out.println("Here are the people invited!");
             readList(list);
         }
